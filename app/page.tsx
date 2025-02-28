f93efd8e1d0dcce22f38328275a0282eb4a1da9d
Abdul Khaliq <abdulkhaliqise2023@gmail.com>
2025-02-28 21:38:25 +0530

    Removed console.log

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,6 @@ const Home = () => {
     setErrors(newErrors);
     return isValid;
   };
-  console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
